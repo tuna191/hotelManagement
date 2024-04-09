fd4b95a82416a0417030d86c4a3ef79a95e61704
Tai Sềnh Vấy <109068895+doivi@users.noreply.github.com>
2024-04-09 18:49:29 +0700

    add bill detailbill customer

---
+++ hotelManagement/src/model/Bill/BillInterface.java
@@ -0,0 +1,14 @@
+package model.Bill;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.List;
+
+public interface BillInterface extends Remote {
+    public List<Bill> getBill(String idBill) throws RemoteException;
+    public List<Bill> getAllBill() throws RemoteException;
+    public boolean addBill(Bill bill) throws RemoteException;
+    public boolean removeBill(String idBill) throws RemoteException;
+    public boolean updateBill(Bill bill) throws RemoteException;
+}
+
